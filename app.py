d8604227521220dab536e30cc80dc5127be467a8
Abhishekmudda <158270613+Abhishekmudda@users.noreply.github.com>
2025-01-03 21:20:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from groq import Groq
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -15,22 +16,20 @@ from datetime import date
 
 os.system("bash setup.sh")
 
-db_dir = rdb_dir = r"./chroma_db_directory"
-os.makedirs(db_dir, exist_ok=True)
+#db_dir = rdb_dir = r"./chroma_db_directory"
+#os.makedirs(db_dir, exist_ok=True)
 lamma_new = "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"
 
 
 def setup_driver():
-    options = webdriver.ChromeOptions()
+    options = Options()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-notifications')
     options.add_argument('--disable-blink-features=AutomationControlled')
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
-    service = Service(ChromeDriverManager().install())
-    return webdriver.Chrome(service=service, options=options)
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     
 def Hotels(checkin_date, checkout_date, location):
     driver = setup_driver()
